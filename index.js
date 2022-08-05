5622a312f714be2cb60ec444d822033e06cf70a6
MURRAY122 <68895757+MURRAY122@users.noreply.github.com>
2022-08-05 18:39:57 +0000

    js updated and layout change

--- index.js
+++ index.js
@@ -1,32 +1,47 @@
 const ratingBox = document.getElementById('ratingContent');
 const thanksBox = document.getElementById('thanksContent');
-const rating = document.getElementById('rating');
+const ratingsBtns = document.querySelectorAll(".section__content__ratings__btn");
+const ratingSubmit = document.getElementById('ratingSubmit');
+const ratingMessage = document.getElementById('rating');
+let rating = 0;
 
-
-
-// Display thank you message content
-function setRating(element){
-    //Remove any rating-active class incase user changes rating value 
-    const oldRating = document.getElementsByClassName('rating-active');
-    if(oldRating.length != 0){
-    oldRating[0].classList.remove('rating-active');
-    }
-    //Hide rating box
-    element.classList.add('rating-active');
+// Set selected rating
+function setRating(rateElement){
+    [].map.call(ratingsBtns, function(element) {
+        element.classList.remove('rating-active');
+        if(rateElement == element.innerHTML){
+            rating = element.innerHTML;
+            element.classList.add('rating-active');
+        }
+    });
 }
 
 // Display thank you message content
 function submitRating(){
-    const activeRatings = document.getElementsByClassName('rating-active');
-    //If no dom element has rating-active class alert user or display thank you
-    if(activeRatings.length == 0){
+    //If no rating is set, alert user
+    if(rating > 5 || rating <= 0){
     alert('Please select a rating before submitting')
     } else {
         // Set rating on thank you box content
-        rating.innerHTML = activeRatings[0].value;
+        ratingMessage.innerHTML = rating;
         //Hide rating box
         ratingBox.classList.add('hide');
         //Show thanks content
         thanksBox.classList.remove('hide');
     }
 }
+
+// Start and add event listeners
+function main(){
+[].map.call(ratingsBtns, function(element) {
+    element.addEventListener("click",function (){ 
+        setRating(element.innerHTML);
+    });
+});
+
+ratingSubmit.addEventListener("click",function (){ 
+    submitRating();
+});
+}
+
+main();
